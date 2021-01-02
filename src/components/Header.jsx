62f69f6f724f4bcc193adeb9822a3cf1b2809e79
Alexandr <a_s_bas@mail.ru>
2021-01-03 01:29:19 +0300

    lesson 12 components

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header className='header'>
+            <div>
+                <img src="https://wallsdesk.com/wp-content/uploads/2017/01/Arizona-Cardinals-Images.jpg"></img>
+          Header
+        </div>
+        </header>
+    )
+}
+
+export default Header;
+
---
+++ src/components/Profile.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className='content'>
+            <div>
+                <img src='https://im0-tub-ru.yandex.net/i?id=9a27816f66e2db9285bc9c76fa3a8d86&n=13'></img>
+            </div>
+            <div>
+                Ava + description
+      </div>
+            <div>
+                My posts
+        <div>
+                    New post
+        </div>
+                <div>
+                    <div>
+                        Post1
+          </div>
+                    <div>
+                        Post2
+          </div>
+                </div>
+            </div>
+        </div>
+
+    )
+}
+
+export default Profile;
