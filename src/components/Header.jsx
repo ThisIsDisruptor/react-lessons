6e5ab8659f779b5f7c90ca1e3d0ac51d9452e8d6
Alexandr <a_s_bas@mail.ru>
2021-01-03 17:16:52 +0300

    lesson 14 css-modules

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
+import classes from './Header.module.css';
 
 const Header = () => {
     return (
-        <header className='header'>
+        <header className={classes.header}>
             <div>
                 <img src="https://wallsdesk.com/wp-content/uploads/2017/01/Arizona-Cardinals-Images.jpg"></img>
           Header
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
+import  classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={classes.content}>
             <div>
                 <img src='https://im0-tub-ru.yandex.net/i?id=9a27816f66e2db9285bc9c76fa3a8d86&n=13'></img>
             </div>
             <div>
                 Ava + description
-      </div>
+            </div>
             <div>
                 My posts
-        <div>
-                    New post
-        </div>
-                <div>
                     <div>
+                    New post
+                    </div>
+                <div className={classes.posts}>
+                    <div className={classes.item}>
                         Post1
-          </div>
+                    </div>
                     <div>
                         Post2
-          </div>
+                    </div>
                 </div>
             </div>
         </div>
