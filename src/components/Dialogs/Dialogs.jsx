40f637fa78880356e4f50b0379df3ce2b8bef960
Alexandr <a_s_bas@mail.ru>
2021-01-04 19:12:13 +0300

    lesson 21 dialogs page modified

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
-import  classes from './Dialogs.module.css';
+import classes from './Dialogs.module.css';
 
 const Dialogs = (props) => {
     return (
-        <div>
-            Dialogs
+        <div className={classes.dialogs}>
+            <div className={classes.dialogsItems}>
+                <div className={classes.dialog + ' ' + classes.active}>
+                    Dimas
+                </div>
+                <div className={classes.dialog}>
+                    Dryus
+                </div>
+                <div className={classes.dialog}>
+                    Zaxar
+                </div>
+                <div className={classes.dialog}>
+                    Pus
+                </div>
+
+            </div>
+            <div className={classes.messsages}>
+                <div className={classes.message}>Hello</div>
+                <div className={classes.message}>How are you</div>
+                <div className={classes.message}>Yo! </div>
+
+            </div>
         </div>
     )
 }
