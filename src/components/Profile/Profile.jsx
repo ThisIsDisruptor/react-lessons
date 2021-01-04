a0023af7cf504e13dc5dee26d9d23da60b523321
Alexandr <a_s_bas@mail.ru>
2021-01-04 16:48:12 +0300

    lesson 18 added dialogs page

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -4,7 +4,7 @@ import  classes from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className={classes.content}>
+        <div>
             <div>
                 <img src='https://im0-tub-ru.yandex.net/i?id=9a27816f66e2db9285bc9c76fa3a8d86&n=13'></img>
             </div>
