0277de49711e54e9b7a5dae3fe98e946ee04005a
Alexander <alexander@Acer>
2021-01-14 02:13:36 +0300

    lesson 56 userAPIContainer mover yo UserContainer

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -6,64 +6,74 @@ import defaultAva from "../../assets/images/default_ava.jpg";
 let Users = (props) => {
   let users = props.usersPage.users;
 
-  let getUsers = () => {
-    if (users.length === 0) {
-      axios
-        .get("https://social-network.samuraijs.com/api/1.0/users")
-        .then((response) => {
-          debugger;
-          props.setUsers(response.data.items);
-        });
-    }
-  };
+  let pagesCount = Math.ceil(
+    props.usersPage.totalUsersCount / props.usersPage.pageSize
+  );
 
-  let userElements = users.map((user) => (
-    <div key={user.id} className={classes.user}>
-      <span>
-        <div>
-          <img
-            src={user.photos.small != null ? user.photos.small : defaultAva}
-            alt=""
-          />
-        </div>
-        <div>
-          {user.followed ? (
-            <button
-              onClick={() => {
-                props.unfollowUser(user.id);
-              }}
-            >
-              Unfollow
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                props.followUser(user.id);
-              }}
-            >
-              Follow
-            </button>
-          )}
-        </div>
-      </span>
-      <span>
-        <span>
-          <div>{user.name}</div>
-          <div>{user.status}</div>
-        </span>
-        <span>
-          <div>"user.location.country"</div>
-          <div>"user.location.city"</div>
-        </span>
-      </span>
-    </div>
-  ));
+  let currentPage = props.usersPage.currentPage;
+
+  let pages = [];
+
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i);
+  }
 
   return (
     <div>
-      Users will be here
-      <button onClick={getUsers()}>Get Users</button>
-      {userElements}
+      Users are here
+      <div>
+        {pages.map((page) => (
+          <span
+            className={page === currentPage ? classes.selectedPage : ""}
+            onClick={(e) => {
+              props.onPageChanged(page);
+            }}
+          >
+            {page + " "}
+          </span>
+        ))}
+      </div>
+      {users.map((user) => (
+        <div key={user.id} className={classes.user}>
+          <span>
+            <div>
+              <img
+                src={user.photos.small != null ? user.photos.small : defaultAva}
+                alt=""
+              />
+            </div>
+            <div>
+              {user.followed ? (
+                <button
+                  onClick={() => {
+                    props.unfollowUser(user.id);
+                  }}
+                >
+                  Unfollow
+                </button>
+              ) : (
+                <button
+                  onClick={() => {
+                    props.followUser(user.id);
+                  }}
+                >
+                  Follow
+                </button>
+              )}
+            </div>
+          </span>
+          <span>
+            <span>
+              <div>{user.name}</div>
+              <div>{user.status}</div>
+            </span>
+            <span>
+              <div>"user.location.country"</div>
+              <div>"user.location.city"</div>
+            </span>
+          </span>
+        </div>
+      ))}
     </div>
   );
 };
--- src/components/Users/UsersContainer.jsx
+++ src/components/Users/UsersContainer.jsx
@@ -7,8 +7,47 @@ import {
   setUsersActionCreator,
   unfollowtActionCreator,
 } from "../../redux/usersReducer";
+import * as axios from "axios";
 import Users from "./Users";
-import UsersClassComponent from "./UsersClassComponent";
+
+class UsersAPIComponent extends React.Component {
+  componentDidMount() {
+    let users = this.props.usersPage.users;
+
+    if (users.length === 0) {
+      axios
+        .get(
+          `https://social-network.samuraijs.com/api/1.0/users?page=${this.props.usersPage.currentPage}&count=${this.props.usersPage.pageSize}`
+        )
+        .then((response) => {
+          this.props.setUsers(response.data.items);
+          this.props.setTotalUsersCount(response.data.totalCount);
+        });
+    }
+  }
+
+  onPageChanged = (pageNumber) => {
+    this.props.setCurrentPage(pageNumber);
+    axios
+      .get(
+        `https://social-network.samuraijs.com/api/1.0/users?page=${pageNumber}&count=${this.props.usersPage.pageSize}`
+      )
+      .then((response) => {
+        this.props.setUsers(response.data.items);
+      });
+  };
+
+  render() {
+    return (
+      <Users
+        usersPage={this.props.usersPage}
+        onPageChanged={this.onPageChanged}
+        followUser={this.props.followUser}
+        unfollowUser={this.props.unfollowUser}
+      />
+    );
+  }
+}
 
 const mapStateToProps = (state) => {
   return {
@@ -43,6 +82,6 @@ const mapDispatchToProps = (dispatch) => {
 const UsersContainer = connect(
   mapStateToProps,
   mapDispatchToProps
-)(UsersClassComponent);
+)(UsersAPIComponent);
 
 export default UsersContainer;
