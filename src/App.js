7e205bd4b2808e8dab3c539c4fe868415f515929
Alexandr <a_s_bas@mail.ru>
2021-02-06 15:21:08 +0300

    lesson 95 browserRouter baseName

--- src/App.js
+++ src/App.js
@@ -1,83 +1,73 @@
-import React, { Suspense } from "react";
-import "./App.css";
-import Navbar from "./components/Navbar/Navbar";
-import { BrowserRouter, Route, withRouter } from "react-router-dom";
-import News from "./components/News/News";
-import Music from "./components/Music/Music";
-import Settings from "./components/Settings/Settings";
-import UsersContainer from "./components/Users/UsersContainer";
-import HeaderContainer from "./components/Header/HeaderContainer";
-import LoginContainer from "./components/Login/LoginContainer";
-import { connect, Provider } from "react-redux";
-import { compose } from "redux";
-import { initializeAppThunkCreator } from "./redux/appReducer";
-import Preloader from "./components/common/Preloader/Preloader";
-import store from "./redux/reduxStore";
-import { withSuspense } from "./hoc/withSuspense";
+import React, { Suspense } from 'react';
+import './App.css';
+import Navbar from './components/Navbar/Navbar';
+import { BrowserRouter, Route, withRouter } from 'react-router-dom';
+import News from './components/News/News';
+import Music from './components/Music/Music';
+import Settings from './components/Settings/Settings';
+import UsersContainer from './components/Users/UsersContainer';
+import HeaderContainer from './components/Header/HeaderContainer';
+import LoginContainer from './components/Login/LoginContainer';
+import { connect, Provider } from 'react-redux';
+import { compose } from 'redux';
+import { initializeAppThunkCreator } from './redux/appReducer';
+import Preloader from './components/common/Preloader/Preloader';
+import store from './redux/reduxStore';
+import { withSuspense } from './hoc/withSuspense';
 
-const DialogsContainer = React.lazy(() =>
-  import("./components/Dialogs/DialogsContainer")
-);
+const DialogsContainer = React.lazy(() => import('./components/Dialogs/DialogsContainer'));
 
-const ProfileContainer = React.lazy(() =>
-  import("./components/Profile/ProfileContainer")
-);
+const ProfileContainer = React.lazy(() => import('./components/Profile/ProfileContainer'));
 
 class AppComponent extends React.Component {
-  componentDidMount() {
-    this.props.initializeApp();
-  }
+	componentDidMount() {
+		this.props.initializeApp();
+	}
 
-  render() {
-    let sideBar = this.props.state.sideBar;
-    if (!this.props.state.app.initialized) {
-      return <Preloader isFetching={true} />;
-    }
+	render() {
+		let sideBar = this.props.state.sideBar;
+		if (!this.props.state.app.initialized) {
+			return <Preloader isFetching={true} />;
+		}
 
-    return (
-      <div className="app-wrapper">
-        <HeaderContainer />
-        <Navbar sideBar={sideBar} />
-        <div className="app-wrapper-content">
-          <Route
-            path="/profile/:userId?"
-            render={withSuspense(ProfileContainer)}
-          />
-          <Route path="/dialogs" render={withSuspense(DialogsContainer)} />
+		return (
+			<div className="app-wrapper">
+				<HeaderContainer />
+				<Navbar sideBar={sideBar} />
+				<div className="app-wrapper-content">
+					<Route path="/profile/:userId?" render={withSuspense(ProfileContainer)} />
+					<Route path="/dialogs" render={withSuspense(DialogsContainer)} />
 
-          <Route path="/news" component={News} />
-          <Route path="/music" component={Music} />
-          <Route path="/settings" component={Settings} />
+					<Route path="/news" component={News} />
+					<Route path="/music" component={Music} />
+					<Route path="/settings" component={Settings} />
 
-          <Route path="/users" render={() => <UsersContainer />} />
+					<Route path="/users" render={() => <UsersContainer />} />
 
-          <Route path="/login" render={() => <LoginContainer />} />
-        </div>
-      </div>
-    );
-  }
+					<Route path="/login" render={() => <LoginContainer />} />
+				</div>
+			</div>
+		);
+	}
 }
 
-let mapStateToProps = (state) => ({
-  state,
+let mapStateToProps = state => ({
+	state,
 });
 
 let functionsToProps = {
-  initializeApp: initializeAppThunkCreator,
+	initializeApp: initializeAppThunkCreator,
 };
 
-let AppContainer = compose(
-  withRouter,
-  connect(mapStateToProps, functionsToProps)
-)(AppComponent);
+let AppContainer = compose(withRouter, connect(mapStateToProps, functionsToProps))(AppComponent);
 
-let SocialNetworkApp = (props) => (
-  <React.StrictMode>
-    <BrowserRouter>
-      <Provider store={store}>
-        <AppContainer store={store} />
-      </Provider>
-    </BrowserRouter>
-  </React.StrictMode>
+let SocialNetworkApp = props => (
+	<React.StrictMode>
+		<BrowserRouter basename={process.env.PUBLIC_URL}>
+			<Provider store={store}>
+				<AppContainer store={store} />
+			</Provider>
+		</BrowserRouter>
+	</React.StrictMode>
 );
 export default SocialNetworkApp;
